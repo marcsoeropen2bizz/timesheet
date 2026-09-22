78183b8d47f8d5c10c77f72c6fb1d11f5517601c
Marc Soer <m.soer@open2bizz.nl>
2026-09-22 09:24:21 +0200

    [MIG] replace old owl widget

--- hr_timesheet_sheet/static/src/components/timesheet_matrix/timesheet_matrix.esm.js
+++ hr_timesheet_sheet/static/src/components/timesheet_matrix/timesheet_matrix.esm.js
@@ -1,12 +1,19 @@
+import {DateTime} from "luxon";
+
+import {registry} from "@web/core/registry";
+import {
+    X2Many2DMatrixField,
+    x2Many2DMatrixField,
+} from "@web_widget_x2many_2d_matrix/components/x2many_2d_matrix_field/x2many_2d_matrix_field.esm";
 import {X2Many2DMatrixRenderer} from "@web_widget_x2many_2d_matrix/components/x2many_2d_matrix_renderer/x2many_2d_matrix_renderer.esm";
-import {patch} from "@web/core/utils/patch";
 
-console.log("HR TIMESHEET MATRIX PATCH LOADED");
 
-patch(X2Many2DMatrixRenderer.prototype, {
+export class TimesheetMatrixRenderer extends X2Many2DMatrixRenderer {
+    static template = "hr_timesheet_sheet.TimesheetMatrixRenderer";
+
     _getColumns(records) {
         const columns = super._getColumns(...arguments);
-        const today = luxon.DateTime.now().toISODate();
+        const today = DateTime.now().toISODate();
         const allRecords = records || this.list.records;
 
         for (const col of columns) {
@@ -21,32 +28,52 @@ patch(X2Many2DMatrixRenderer.prototype, {
             });
 
             if (record?.data?.date) {
+                const date = record.data.date;
                 const dateStr =
-                    record.data.date.toISODate?.() ||
-                    record.data.date.split(" ")[0];
+                    date.toISODate?.() ||
+                    (typeof date === "string" ? date.split(" ")[0] : null);
+
                 col.isToday = dateStr === today;
             }
         }
+
         return columns;
-    },
+    }
 
     _getCellClass(column, rowValue) {
         const x = this.columns.findIndex((c) => c.value === column.value);
         const y = this.rows.findIndex((r) => r.value === rowValue);
         const val = this.matrix[y]?.[x]?.value || 0;
+
         return {
             o_matrix_today: column.isToday,
             o_matrix_value_nonzero: val > 0,
             o_matrix_value_zero: val === 0,
         };
-    },
+    }
 
     _getColumnTotalClass(column) {
-        return {o_matrix_today: column.isToday};
-    },
-});
-console.log(
-    "PATCH RESULT",
-    typeof X2Many2DMatrixRenderer.prototype._getCellClass,
-    X2Many2DMatrixRenderer.prototype
-);
+        return {
+            o_matrix_today: column.isToday,
+        };
+    }
+}
+
+
+export class TimesheetMatrixField extends X2Many2DMatrixField {
+    static components = {
+        ...X2Many2DMatrixField.components,
+        X2Many2DMatrixRenderer: TimesheetMatrixRenderer,
+    };
+}
+
+
+export const timesheetX2Many2DMatrixField = {
+    ...x2Many2DMatrixField,
+    component: TimesheetMatrixField,
+};
+
+
+registry
+    .category("fields")
+    .add("timesheet_x2many_2d_matrix", timesheetX2Many2DMatrixField);
