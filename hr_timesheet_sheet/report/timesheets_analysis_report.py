db5d58b6aad4dd7aaad7f20e74e014afa7d78226
Marc Soer <m.soer@open2bizz.nl>
2026-09-22 08:40:45 +0200

    [MIG] update timesheet_analysis_report.py with new SQL-api

--- hr_timesheet_sheet/report/timesheets_analysis_report.py
+++ hr_timesheet_sheet/report/timesheets_analysis_report.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-
+from odoo.tools import SQL
 
 class TimesheetsAnalysisReport(models.Model):
     _inherit = "timesheets.analysis.report"
